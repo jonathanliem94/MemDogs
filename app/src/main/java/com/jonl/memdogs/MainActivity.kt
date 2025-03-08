6a0a130f67b1579ca7689706cf2cd75ee16ef710
Jonathan L <14284080+jonathanliem94@users.noreply.github.com>
2025-03-08 19:55:36 +0800

    feat: added basic start screen and nav

--- app/src/main/java/com/jonl/memdogs/MainActivity.kt
+++ app/src/main/java/com/jonl/memdogs/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.jonl.memdogs.ui.MainBottomNavigationBar
 import com.jonl.memdogs.ui.theme.MemDogsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MemDogsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MemDogsTheme {
-        Greeting("Android")
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = { MainBottomNavigationBar(navController) }
+    ) { paddingValues ->
+        MainNavGraph(
+            navController = navController,
+            modifier = Modifier.padding(paddingValues),
+        )
     }
 }
---
+++ app/src/main/java/com/jonl/memdogs/ui/memorytest/MemoryTestScreen.kt
@@ -0,0 +1,11 @@
+package com.jonl.memdogs.ui.memorytest
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MemoryTestScreen() {
+    Text(
+        text = "Memory Test"
+    )
+}
